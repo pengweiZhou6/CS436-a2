d42e6ceac33641e83b24692f173ac578ddfb68a2
w3h2b <Zpw@37201769>
2020-07-19 02:21:12 -0700

    13

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json())
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 console.log("port",process.env.PORT);
 if (process.env.NODE_ENV === 'production'){
-    app.use(express.static('my-app/build'))
+    // app.use(express.static('my-app/build'))
     //
     // app.get('*', (req, res) => {
     //     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); // relative path
