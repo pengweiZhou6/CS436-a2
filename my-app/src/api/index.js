19dd26bec15b506cf0489ad3b5a4710a3b2b1612
w3h2b <Zpw@37201769>
2020-07-19 17:32:42 -0700

    19

--- my-app/src/api/index.js
+++ my-app/src/api/index.js
@@ -1,23 +1,11 @@
 import axios from 'axios'
-const aws = require('aws-sdk');
-let s3 = new aws.S3({
-    port: process.env.PORT,
-});
 
-const apiPort = s3.port || 8000
-
-
-const api = axios.create({
-    baseURL: 'http://localhost:'+ apiPort +'/api',
-})
-
-
-export const insertMsg = payload => api.post(`/message`, payload)
-export const getAllMsg = () => api.get(`/messages`)
-export const updateMsgById = (id, payload) => api.put(`/message/${id}`, payload)
-export const deleteMsgById = id => api.delete(`/message/${id}`)
-export const deleteAllMsg = () => api.delete(`/messages`)
-export const getMsgById = id => api.get(`/message/${id}`)
+export const insertMsg = payload => api.post(`/api/message`, payload)
+export const getAllMsg = () => axios.get('/api/messages')
+export const updateMsgById = (id, payload) => api.put(`/api/message/${id}`, payload)
+export const deleteMsgById = id => api.delete(`/api/message/${id}`)
+export const deleteAllMsg = () => api.delete(`/api/messages`)
+export const getMsgById = id => api.get(`/api/message/${id}`)
 
 const apis = {
     insertMsg,
