95c4714357c87ed0ca3f4afff5afe2b540cc4888
w3h2b <Zpw@37201769>
2020-07-19 17:37:58 -0700

    20

--- my-app/src/api/index.js
+++ my-app/src/api/index.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
 
-export const insertMsg = payload => api.post(`/api/message`, payload)
+export const insertMsg = payload => axios.post(`/api/message`, payload)
 export const getAllMsg = () => axios.get('/api/messages')
-export const updateMsgById = (id, payload) => api.put(`/api/message/${id}`, payload)
-export const deleteMsgById = id => api.delete(`/api/message/${id}`)
-export const deleteAllMsg = () => api.delete(`/api/messages`)
-export const getMsgById = id => api.get(`/api/message/${id}`)
+export const updateMsgById = (id, payload) => axios.put(`/api/message/${id}`, payload)
+export const deleteMsgById = id => axios.delete(`/api/message/${id}`)
+export const deleteAllMsg = () => axios.delete(`/api/messages`)
+export const getMsgById = id => axios.get(`/api/message/${id}`)
 
 const apis = {
     insertMsg,
